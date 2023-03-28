b58abc6783e1d948381648f4849a43aa6e2ffdd9
Hawkeye <swarup@agpaytech.co.uk>
2023-03-28 20:46:51 +0530

    index page added

---
+++ templates/Pages/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @var App\View\AppView $this
+ */
+?>
+
+<section id="hero" class="hero d-flex align-items-center top-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 d-flex flex-column justify-content-center">
+                <h1 data-aos="fade-up" class="mb-4">Lorem Ipsum is simply
+                    dummy</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s
+                </p>
+                <div data-aos="fade-up" data-aos-delay="600">
+                    <div class="text-lg-start">
+                        <a href="#" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                            <span><?php echo __('Contact Us') ?></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <img src="img/BannerImg.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
