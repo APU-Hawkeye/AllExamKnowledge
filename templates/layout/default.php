31d7da5c6239adce5a318d8558f199751b930331
Hawkeye <swarup@agpaytech.co.uk>
2023-03-28 20:47:26 +0530

    default layout modified

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,6 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,36 +21,44 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        <?= $cakeDescription ?>:
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+    <?php echo $this->Html->css([
+        '/lib/bootstrap/css/bootstrap.min',
+        '/lib/bootstrap-icons/bootstrap-icons',
+        '/lib/flagstrap/css/flags',
+        'style',
+    ])?>
+    <?php echo $this->fetch('css')?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+<!-- ======= Header ======= -->
+<?php echo $this->element("header"); ?>
+<!-- End Header -->
+
+<?php echo $this->fetch("content"); ?>
+
+<!-- ======= Footer ======= -->
+<?php echo $this->element("footer"); ?>
+<!-- End Footer -->
+
+<!--    <nav class="top-nav">-->
+<!--        -->
+<!--    </nav>-->
+<!--    <main class="main">-->
+<!--        <div class="container">-->
+<!--            --><?//= $this->Flash->render() ?>
+<!--            --><?//= $this->fetch('content') ?>
+<!--        </div>-->
+<!--    </main>-->
+<!--    <footer>-->
+<!--    </footer>-->
 </body>
 </html>
