739f6a8507e6454042d1fa0151084e566af74c1c
Hawkeye <swarup@agpaytech.co.uk>
2023-04-03 14:35:00 +0530

    authentication logic added in src application

--- src/Application.php
+++ src/Application.php
@@ -16,6 +16,11 @@ declare(strict_types=1);
  */
 namespace App;
 
+use Authentication\AuthenticationService;
+use Authentication\AuthenticationServiceInterface;
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Identifier\IdentifierInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
 use Cake\Core\Configure;
 use Cake\Core\ContainerInterface;
 use Cake\Datasource\FactoryLocator;
@@ -23,10 +28,14 @@ use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Cake\Http\BaseApplication;
 use Cake\Http\Middleware\BodyParserMiddleware;
 use Cake\Http\Middleware\CsrfProtectionMiddleware;
+use Cake\Http\Middleware\EncryptedCookieMiddleware;
 use Cake\Http\MiddlewareQueue;
+use Cake\Http\ServerRequest;
 use Cake\ORM\Locator\TableLocator;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
+use Cake\Routing\Router;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Application setup class.
@@ -34,7 +43,7 @@ use Cake\Routing\Middleware\RoutingMiddleware;
  * This defines the bootstrapping logic and middleware layers you
  * want to use in your application.
  */
-class Application extends BaseApplication
+class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
     /**
      * Load all the application configuration and bootstrap logic.
@@ -64,6 +73,7 @@ class Application extends BaseApplication
         }
 
         // Load more plugins here
+        $this->addPlugin('Authentication');
     }
 
     /**
@@ -74,6 +84,15 @@ class Application extends BaseApplication
      */
     public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
     {
+//        $csrf = new CsrfProtectionMiddleware([
+//            'httponly' => true
+//        ]);
+//        $csrf->skipCheckCallback(function (ServerRequest $request) {
+//            if ($request->getParam('prefix') === 'Admin' || $request->getParam("controller") === "Users") {
+//                return true;
+//            }
+//        });
+
         $middlewareQueue
             // Catch any exceptions in the lower layers,
             // and make an error page/response
@@ -83,6 +102,9 @@ class Application extends BaseApplication
             ->add(new AssetMiddleware([
                 'cacheTime' => Configure::read('Asset.cacheTime'),
             ]))
+            ->add(new EncryptedCookieMiddleware([
+                'csrfToken'
+            ], Configure::read('Security.CookieKey', env('COOKIE_KEY'))))
 
             // Add routing middleware.
             // If you have a large number of routes connected, turning on routes
@@ -99,9 +121,8 @@ class Application extends BaseApplication
 
             // Cross Site Request Forgery (CSRF) Protection Middleware
             // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
-            ->add(new CsrfProtectionMiddleware([
-                'httponly' => true,
-            ]));
+//            ->add($csrf)
+            ->add(new AuthenticationMiddleware($this));
 
         return $middlewareQueue;
     }
@@ -132,5 +153,41 @@ class Application extends BaseApplication
         $this->addPlugin('Migrations');
 
         // Load more plugins here
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return AuthenticationServiceInterface
+     */
+    public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface
+    {
+        $service = new AuthenticationService();
+        $service->loadAuthenticator('Authentication.Session');
+        if ($request->getParam('prefix') === 'Admin') {
+            $service->setConfig([
+                'unauthenticatedRedirect' => Router::url([
+                    'controller' => 'Users',
+                    'action' => 'login',
+                    'prefix' => 'Admin',
+                    'plugin' => null
+                ]),
+                'queryParam' => 'referer',
+            ]);
+            $fields = [
+                IdentifierInterface::CREDENTIAL_USERNAME => 'username',
+                IdentifierInterface::CREDENTIAL_PASSWORD => 'password'
+            ];
+            $service->loadAuthenticator('Authentication.Form', [
+                'fields' => $fields,
+                'loginUrl' => Router::url([
+                    'controller' => 'Users',
+                    'action' => 'login',
+                    'prefix' => 'Admin',
+                    'plugin' => null
+                ])
+            ]);
+            $service->loadIdentifier('Authentication.Password', compact('fields'));
+        }
+        return $service;
     }
 }
