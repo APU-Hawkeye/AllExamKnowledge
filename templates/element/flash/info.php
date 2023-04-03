ccabc29e04fc11f6ab1106c66c2a9fc70f984737
Hawkeye <swarup@agpaytech.co.uk>
2023-04-03 14:33:07 +0530

    flash elements modified

--- templates/element/flash/info.php
+++ templates/element/flash/info.php
@@ -8,4 +8,12 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<div class="alert alert-light-info alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+    <div class="alert-icon">
+        <i class="icon-close"></i>
+    </div>
+    <div class="alert-message">
+        <span><?php echo $message; ?></span>
+    </div>
+</div>
--- templates/element/flash/success.php
+++ templates/element/flash/success.php
@@ -8,4 +8,12 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message success" onclick="this.classList.add('hidden')"><?= $message ?></div>
+<div class="alert alert-outline-success alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+    <div class="alert-icon">
+        <i class="icon-check"></i>
+    </div>
+    <div class="alert-message">
+        <span><?php echo $message; ?></span>
+    </div>
+</div>
