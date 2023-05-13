ead7e71756a1ed5af68e855eac53917065c613d9
Hawkeye <swarup@agpaytech.co.uk>
2023-05-13 16:07:26 +0530

    footer added

---
+++ templates/element/footer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @var App\View\AppView $this
+ */
+?>
+<style>
+    .col-md-2 {
+        -ms-flex: 0 0 10.666667%;
+        flex: 0 0 10.666667%;
+    }
+    .heading {
+        font-family: Helvetica;
+    }
+</style>
+<footer id="footer" class="footer">
+    <div class="footer-top">
+        <div class="container">
+            <h1 class="heading pb-5"><?php echo __('Stay In Touch')?></h1>
+            <form class="form-inline justify-content-center">
+                <div class="form-group mr-sm-3 mb-2">
+                    <input type="text" class="form-control" placeholder="john@example.com">
+                    <button type="submit" class="btn btn-outline-secondary pl-2"><?php echo __('Subscibe')?></button>
+                </div>
+            </form>
+        </div><!---->
+    </div><!--//footer top-->
+    <div class="footer-bottom">
+        <div class="container pt-4">
+            <div class="row justify-content-center">
+                <div class="col-md-2">
+                    <p><a href="#" class="text-white"><?php echo __('Home')?></a></p>
+                </div>
+                <div class="col-md-2">
+                    <p><a href="#" class="text-white"><?php echo __('About Us')?></a></p>
+                </div>
+                <div class="col-md-2">
+                    <p><a href="#" class="text-white"><?php echo __('Videos')?></a></p>
+                </div>
+                <div class="col-md-2">
+                    <p><a href="#" class="text-white"><?php echo __('Downloads')?></a></p>
+                </div>
+                <div class="col-md-2">
+                    <p><a href="#" class="text-white"><?php echo __('About Us')?></a></p>
+                </div>
+                <div class="col-md-2">
+                    <p><a href="#" class="text-white"><?php echo __('Contact Us')?></a></p>
+                </div>
+            </div>
+            <div class="row mt-5 justify-content-center">
+                <h1 class="heading"><?php echo __('Get In touch with us')?></h1>
+            </div>
+            <div class="row mt-3 mb-5 justify-content-center">
+                <div class="col-md-3">
+                    <p><img src="img/whats-app-icon.svg" alt="">  (+91)9876543210</p>
+                </div>
+                <div class="col-md-3">
+                    <p>contact@allexamknowledge.com</p>
+                </div>
+            </div>
+            <hr width="90%" align="middle" color="#0f6674" />
+            <div class="row mt-4 justify-content-center">
+                <p>Copyright 2022 @copyright allexamknowledge Pvt. Ltd.</p>
+            </div>
+        </div>
+    </div>
+</footer>
