682b0ffb67d141d333be692f8542af7c8e9fe0d5
Hawkeye <swarup@agpaytech.co.uk>
2023-05-13 16:09:48 +0530

    app controller added for administration

---
+++ src/Controller/Admin/AppController.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\AppController as BaseController;
+use Authentication\Controller\Component\AuthenticationComponent;
+use Cake\Event\EventInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class AppController
+ * @package App\Controller\Admin
+ *
+ * @property AuthenticationComponent $Authentication
+ * @property ResponseInterface $response
+ */
+class AppController extends BaseController
+{
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Authentication.Authentication');
+    }
+
+    /**
+     * @param EventInterface $event
+     * @return \Cake\Http\Response|void|null
+     */
+    public function beforeFilter(EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        $this->FormProtection->setConfig([
+            'validate' => false
+        ]);
+    }
+}
