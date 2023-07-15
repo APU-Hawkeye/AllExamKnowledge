697ec5431451d957c77fd3f97f3fe61b8114f71b
Hawkeye <swarup@agpaytech.co.uk>
2023-07-15 17:09:49 +0530

    video section commented

--- templates/Pages/index.php
+++ templates/Pages/index.php
@@ -328,68 +328,68 @@
             </div>
         </div>
     </section>
-    <section class="pb-0 upload-video-section">
-        <div class="container">
-            <div class="row justify-content-center mb-5">
-                <div class="col-lg-8 text-center">
-                    <h2 class="fw-bold text-black">Uploaded Today Video</h2>
-                    <p></p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12 position-relative">
-                    <div class="uploaded-video">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/upzp4xG8kUc" title="Osssc Panchayat Executive Officer and Junior Assistant Exam Expected GK Questions Analysis 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                            <div class="col-lg-6">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/4X7IlmXZNBQ" title="Osssc Panchayat Executive Officer and Junior Assistant Exam 2023: Expected Computer Questions." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="section-bg recent-video-section" >
-        <div class="container">
-            <div class="row justify-content-center mb-5">
-                <div class="col-lg-8 text-center">
-                    <h2 class="fw-bold text-black">Recent Uploaded Video</h2>
-                    <p>Comprehensive Study Material,Expert Faculty at Your Disposal, Interactive Learning Experience, Convenience at Your Fingertips</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="swiper videoSwiper px-3">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/Neq4wyxwHis" title="Osssc Panchayat Executive Officer and Junior Assistant Exam Expected GK Questions Analysis 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                            <div class="swiper-slide">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/BY4o0p9qydY" title="Ossc CGLRE Main Exam 2023 Date Out! Latest Update Notification." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                            <div class="swiper-slide">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/mHU0zfe8ZkM" title="Ossc Chsl Exam 300+ Post Vacancy Exam Pattern Syllabus Details Notification 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                            <div class="swiper-slide">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/mHU0zfe8ZkM" title="Ossc Chsl Exam 300+ Post Vacancy Exam Pattern Syllabus Details Notification 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                            <div class="swiper-slide">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/f9Wnn_mJpgM" title="Ossc Chsl Exam 300+ Post Vacancy Details Notification 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                            <div class="swiper-slide">
-                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/mU9LrU3V8-k" title="&quot;Crack OSSC CGLRE Exam with Current Affairs and Computer Awareness Expected Questions&quot; 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                        <div class="swiper-button-prev"></div>
-                        <div class="swiper-button-next"></div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+<!--    <section class="pb-0 upload-video-section">-->
+<!--        <div class="container">-->
+<!--            <div class="row justify-content-center mb-5">-->
+<!--                <div class="col-lg-8 text-center">-->
+<!--                    <h2 class="fw-bold text-black">Uploaded Today Video</h2>-->
+<!--                    <p></p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="row">-->
+<!--                <div class="col-lg-12 position-relative">-->
+<!--                    <div class="uploaded-video">-->
+<!--                        <div class="row">-->
+<!--                            <div class="col-lg-6">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/upzp4xG8kUc" title="Osssc Panchayat Executive Officer and Junior Assistant Exam Expected GK Questions Analysis 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                            <div class="col-lg-6">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/4X7IlmXZNBQ" title="Osssc Panchayat Executive Officer and Junior Assistant Exam 2023: Expected Computer Questions." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
+<!--    <section class="section-bg recent-video-section" >-->
+<!--        <div class="container">-->
+<!--            <div class="row justify-content-center mb-5">-->
+<!--                <div class="col-lg-8 text-center">-->
+<!--                    <h2 class="fw-bold text-black">Recent Uploaded Video</h2>-->
+<!--                    <p>Comprehensive Study Material,Expert Faculty at Your Disposal, Interactive Learning Experience, Convenience at Your Fingertips</p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="row">-->
+<!--                <div class="col-lg-12">-->
+<!--                    <div class="swiper videoSwiper px-3">-->
+<!--                        <div class="swiper-wrapper">-->
+<!--                            <div class="swiper-slide">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/Neq4wyxwHis" title="Osssc Panchayat Executive Officer and Junior Assistant Exam Expected GK Questions Analysis 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                            <div class="swiper-slide">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/BY4o0p9qydY" title="Ossc CGLRE Main Exam 2023 Date Out! Latest Update Notification." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                            <div class="swiper-slide">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/mHU0zfe8ZkM" title="Ossc Chsl Exam 300+ Post Vacancy Exam Pattern Syllabus Details Notification 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                            <div class="swiper-slide">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/mHU0zfe8ZkM" title="Ossc Chsl Exam 300+ Post Vacancy Exam Pattern Syllabus Details Notification 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                            <div class="swiper-slide">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/f9Wnn_mJpgM" title="Ossc Chsl Exam 300+ Post Vacancy Details Notification 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                            <div class="swiper-slide">-->
+<!--                                <iframe width="100%" height="250" src="https://www.youtube.com/embed/mU9LrU3V8-k" title="&quot;Crack OSSC CGLRE Exam with Current Affairs and Computer Awareness Expected Questions&quot; 2023." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="swiper-button-prev"></div>-->
+<!--                        <div class="swiper-button-next"></div>-->
+<!---->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
 </main>
 
