cd391f5dcb048ad060bc26ef7f0592cc78f6fb4d
Hawkeye <swarup@agpaytech.co.uk>
2023-08-10 17:24:47 +0530

    favicon added

--- templates/Students/login.php
+++ templates/Students/login.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
+    <?php echo $this->Html->meta('favicon.ico', '/img/logo.png', ['type' => 'icon']); ?>
     <title><?php echo __('Login'); ?></title>
 
     <?php echo $this->Html->css([
--- templates/Students/register.php
+++ templates/Students/register.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
+    <?php echo $this->Html->meta('favicon.ico', '/img/logo.png', ['type' => 'icon']); ?>
     <title><?php echo __('Register'); ?></title>
 
     <?php echo $this->Html->css([
