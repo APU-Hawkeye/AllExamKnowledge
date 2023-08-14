a814d6d81fa833fb79eeac0f6bd2303762b88d24
Hawkeye <swarup@agpaytech.co.uk>
2023-08-14 17:38:45 +0530

    privacy policy page added with all information

---
+++ templates/Pages/privacy_policy.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ *
+ */
+?>
+<main id="main" class="mt-5">
+    <section class="faq-section py-5">
+        <div class="container">
+            <h3 class="text-center mb-4 page-header"><?php echo __('Privacy Policy'); ?></h3>
+            <p>
+                <?php echo __('AllExamKnowledge (“Us”, “We,” or “Our”) we value our clients, their information, privacy and trust. This Privacy Policy explains how we collect, use and protect your information through our website on allexamknowledge.com and mobile applications (the “Services). Specifically, this Policy applies to the collection, use, storage, and transfer of information associated with Services. If there is something you do not understand about our privacy practices, please feel free to contact us at contact@allexamknowledge.com'); ?>
+            </p>
+            <p>
+                <?php echo __('Please read this Privacy Policy carefully and ensure that you understand it. Your acceptance of this Privacy Policy is deemed to occur upon your first use of Our Services. If you do not accept and agree with this Privacy Policy, you must stop using our Website immediately.'); ?>
+            </p>
+        </div><!--//container-->
+    </section>
+
+    <section>
+        <div class="container">
+            <h4><?php echo __('1. What This Privacy Policy Covers'); ?></h4>
+            <p>
+                <?php echo __('This Privacy Policy applies only to your use of Our Services. Our Services may contain links to other websites. Please note that we have no control over how your data is collected, stored, or used by other websites and we advise you to check the privacy policies of any such websites before providing any data to them.'); ?>
+            </p>
+            <h4>
+                <?php echo __('2. Meaning of Personal Data'); ?>
+            </h4>
+            <p><?php echo __('Personal data is defined by the General Data Protection Regulation (EU Regulation 2016/679) (the “GDPR”) as ‘any information relating to an identifiable person who can be directly or indirectly identified by reference to an identifier’.'); ?></p>
+            <p>
+                <?php echo __('Personal data is, in simpler terms, any information about you that enables you to be identified. Personal data covers obvious information such as your name and contact details, but it also covers less obvious information such as identification numbers, electronic location data, and other online identifiers.'); ?>
+            </p>
+            <h4><?php echo __('3. Information We Collect'); ?></h4>
+            <p><?php echo __('Depending upon your use of our Services, we may collect some or all of the following personal contact information such as:'); ?></p>
+            <ul class="f-22">
+                <li><?php echo __('Name,'); ?></li>
+                <li><?php echo __('Email address'); ?></li>
+                <li><?php echo __('Telephone number (not mandatory)'); ?></li>
+            </ul>
+            <p>
+                <?php echo __('You will not be able to contact us on our Website without providing this information. This information will not be used for any other purpose unless you subscribe to our newsletter.'); ?>
+            </p>
+            <h4>
+                <?php echo __('4. How We Use your Personal Information'); ?>
+            </h4>
+            <p><?php echo __('Under the GDPR, we must always have a lawful basis for using personal data. This may be because the data is necessary for our performance of a contract with you, because you have consented to our use of your personal data, or because it is in our legitimate business interests to use it.'); ?> </p>
+            <p><?php echo __('With your permission and where permitted by law, we may use your personal data for marketing purposes, which may include contacting you by e-mail with information and news on our services. You will not be sent any unlawful marketing or spam. We will always work to fully protect your rights and comply with our obligations under the GDPR and the Privacy and Electronic Communications (EC Directive) Regulations 2003, and you will always have the opportunity to opt-out.'); ?></p>
+            <h4>
+                <?php echo __('5. Legal Grounds for Using Your Personal Information'); ?>
+            </h4>
+            <p><?php echo __('The law permits us to process your personal data in the way we do because the processing is:'); ?></p>
+            <ul class="f-22">
+                <li><?php echo __('necessary for the purposes of the legitimate interests that we pursue, which are:'); ?></li>
+                <ul>
+                    <li><?php echo __('to run and administer our business;'); ?></li>
+                    <li><?php echo __('to prevent fraud;'); ?></li>
+                    <li><?php echo __('to provide you with our services;'); ?></li>
+                    <li><?php echo __('to evaluate, develop or improve our services;'); ?></li>
+                    <li><?php echo __('to keep you informed about relevant products and services you are using;'); ?></li>
+                    <li><?php echo __('to discharge our legal obligations to store and disclose information where necessary; and/or'); ?></li>
+                    <li><?php echo __('to provide You with an efficient and smooth customer experience;'); ?></li>
+                </ul>
+                <li><?php echo __('necessary for the performance of our contract with You to provide You with our services;'); ?></li>
+                <li><?php echo __('necessary in order to comply with a legal obligation to which we are subject; or'); ?></li>
+                <li><?php echo __('permitted by You, because You have given us consent.'); ?></li>
+                <p>
+                    <?php echo __('Where Your Personal Information is Stored or Transferred'); ?>
+                </p>
+                <p><?php echo __('We share your data with external third parties that are based outside of the EEA.'); ?></p>
+                <p><?php echo __('We will only store and transfer your personal data to countries that the European Commission has deemed to provide an adequate level of personal data protection. More information is available from the European Commission.'); ?></p>
+            </ul>
+
+            <h4><?php echo __('6. Disclosure of Your information'); ?></h4>
+            <p><?php echo __('We will disclose your information to:'); ?></p>
+
+            <ul class="f-22">
+                <li>
+                    <?php echo __('Other third parties, with your consent, whose products and services may be of interest to you;'); ?>
+                </li>
+                <li>
+                    <?php echo __('In the event that we sell or buy any business or assets, to the prospective seller or buyer of such business or assets;'); ?>
+                </li>
+            </ul>
+
+            <h4>
+                <?php echo __(' 7. Links to Other Websites'); ?>
+            </h4>
+            <p><?php echo __('Our website may contain links to other sites, including via our social media buttons. While we try to link only to websites that share our high standards and respect for privacy, we are not responsible for the content, security, or privacy practices employed by other websites and a link does not constitute an endorsement of that website. Once You link to another website from our website You are subject to the terms and conditions of that website, including, but not limited to, its internet privacy policy and practices. Please check these policies before You submit any data to these websites.'); ?></p>
+            <h4><?php echo __('8. Behavioral Targeting / Retargeting'); ?></h4>
+            <p><?php echo __('We partner with a third party to either display advertising on our website or to manage our advertising on other sites. Our third-party partner may use technologies such as cookies to gather information about your activities on this site and other sites in order to provide you advertising based upon your browsing activities and interests. If you wish to not receive this information used for the purpose of serving your interest-based ads, you may opt-out at any time. Otherwise you will continue to receive generic ads.'); ?></p>
+            <h4><?php echo __('9. How We Protect Your Personal Information'); ?></h4>
+            <p><?php echo __('We ensure that there are appropriate technical, physical, electronic, and administrative safeguards in place to protect Your personal details from unauthorised access. Your data is secured by encryption, firewalls and Secure Socket Layer (SSL) technology.  This is industry standard encryption technology which manages the security of messages transmitted across the internet.  When we receive your data, we store it on secure servers which can only be accessed by us.'); ?></p>
+            <h4>
+                <?php echo __('10. How To Control Your Personal Information'); ?>
+            </h4>
+            <ol type="1" class="f-22">
+                <li><?php echo __('In addition to your rights under the GDPR, when you submit personal data via Our Services, you may be given options to restrict our use of your personal data. In particular, we aim to give you strong controls on our use of your data for direct marketing purposes (including the ability to opt-out of receiving emails from us which you may do by unsubscribing using the links provided in our emails. The consent might be revoked at any time by electronic message or by clicking on the “Unsubscribe” link at the end of the commercial information.'); ?>
+                </li>
+                <li>
+                    <?php echo __('You may also wish to sign up to one or more of the preference services that may help to prevent you receiving unsolicited marketing. Please note, however, that these services will not prevent you from receiving marketing communications that you have consented to receiving.'); ?>
+                </li>
+                <li><?php echo __('You may request to review, delete or update your personal information to ensure it is accurate, by contacting us. We aim to respond to your request for access within 30 days.'); ?>
+                </li>
+            </ol>
+            <h4><?php echo __('11. Withholding Information'); ?></h4>
+            <p><?php echo __('You may access our Services without providing any personal data at all. However, to use all features and functions available on Our Services you may be required to submit or allow for the collection of certain data.
+                You may also restrict our use of Cookies.'); ?>
+            </p>
+            <h4><?php echo __('12. How To Access Your Personal Information'); ?></h4>
+            <p><?php echo __('If you want to know what personal information, we have about you, you can ask us for details of that personal data and for a copy of it (where any such personal data is held). This is known as a “subject access request”.'); ?></p>
+            <p><?php echo __('All subject access requests should be made in writing and sent via sales@remitall.co.uk To make this as easy as possible for you, a Subject Access Request Form is available for you to use. You do not have to use this form, but it is the easiest way to tell us everything we need to know to respond to your request as quickly as possible.'); ?></p>
+            <p>
+                <?php echo __('There is not normally any charge for a subject access request. If your request is ‘manifestly unfounded or excessive’ (for example, if you make repetitive requests) a fee may be charged to cover our administrative costs in responding.'); ?>
+            </p>
+            <p>
+                <?php echo __('We will respond to your subject access request within 21 days and, in any case, not more than one month of receiving it. Normally, we aim to provide a complete response, including a copy of your personal data within that time. In some cases, however, particularly if your request is more complex, more time may be required up to a maximum of three months from the date we receive your request. You will be kept fully informed of our progress.'); ?>
+            </p>
+            <h4>
+                <?php echo __('13. Cookies'); ?>
+            </h4>
+            <p>
+                <?php echo __('Our Services may place and access certain first-party Cookies on your computer or device. First-party Cookies are those placed directly by us and are used only by us. We use Cookies to facilitate and improve your experience of Our Services and to provide and improve our services and products. By using Our Services, you may also receive certain third-party Cookies on your computer or device. Third-party Cookies are those placed by websites, services, and/or parties other than us. Third-party Cookies are used on our Services for advertising and analytics services, which also use Cookies. Website analytics refers to a set of tools used to collect and analyse usage statistics, enabling us to better understand how people use Our Services.'); ?>
+            </p>
+            <h4><?php echo __('14. Your Rights'); ?></h4>
+            <p>
+                <?php echo __('Under the GDPR, you have the following rights, which we will always work to uphold:'); ?>
+            </p>
+            <ol type="1" class="f-22">
+                <li>
+                    <?php echo __('The right to be informed about our collection and use of your personal data.'); ?>
+                </li>
+                <li>
+                    <?php echo __('The right to access the personal data we hold about you.'); ?>
+                </li>
+                <li>
+                    <?php echo __('The right to have your personal data rectified if any of your personal data held by us is inaccurate or incomplete.'); ?>
+                </li>
+                <li>
+                    <?php echo __('The right to be forgotten, i.e. the right to ask us to delete or otherwise dispose of any of your personal data that we have.'); ?>
+                </li>
+                <li>
+                    <?php echo __('The right to restrict (i.e. prevent) the processing of your personal data.'); ?>
+                </li>
+                <li>
+                    <?php echo __('The right to object to us using your personal data for a particular purpose or purposes.'); ?>
+                </li>
+                <li>
+                    <?php echo __('The right to data portability. This means that, if you have provided personal data to us directly, we use it with your consent or for the performance of a contract, and that data is processed using automated means, you can ask us for a copy of that personal data to re-use with another service or business in many cases.'); ?>
+                </li>
+                <li>
+                    <?php echo __('Rights relating to automated decision-making and profiling. We do not use your personal data in this way.'); ?>
+                </li>
+                <li>
+                    <?php echo __('For more information about our use of your personal data or exercising your rights as outlined above, please contact us via'); ?>   <span class="text-danger"></span> <?php echo __('sales@remitall.co.uk.'); ?>
+                </li>
+            </ol>
+            <h4>
+                <?php echo __('15. Changes To This Privacy Policy'); ?>
+            </h4>
+            <p>
+                <?php echo __('We may change this Privacy Notice from time to time. This may be necessary, for example, if the law changes, or if we change our business in a way that affects personal data protection.'); ?>
+            </p>
+            <p>
+                <?php echo __('Any changes will be immediately posted on Our Services and you will be deemed to have accepted the terms of the Privacy Policy on your first use of Our Services following the alterations. We recommend that you check this page regularly to keep up to date.'); ?>
+            </p>
+            <h4><?php echo __('16. Contacting Us'); ?></h4>
+            <p>
+                <?php echo __('To contact us about anything to do with your personal data and data protection, including to make a subject access request, please use the following details:'); ?>
+            </p>
+        </div>
+    </section>
+</main><!-- End #main -->
