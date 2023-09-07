47865f2d4a4d3f03d7a11cfeb1250426eb4d8df8
Hawkeye <swarup@agpaytech.co.uk>
2023-09-07 20:28:10 +0530

    Switch behavior attached

--- src/Model/Table/CategoriesTable.php
+++ src/Model/Table/CategoriesTable.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
+use App\Model\Behavior\SwitchBehavior;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
@@ -28,6 +28,7 @@ use Cake\Validation\Validator;
  * @method \App\Model\Entity\Category[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
  *
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ * @mixin SwitchBehavior
  */
 class CategoriesTable extends Table
 {
@@ -46,6 +47,7 @@ class CategoriesTable extends Table
         $this->setPrimaryKey('id');
 
         $this->addBehavior('Timestamp');
+        $this->addBehavior('Switch');
 
         $this->hasMany('StudyMaterials', [
             'foreignKey' => 'category_id',
--- src/Model/Table/SubCategoriesTable.php
+++ src/Model/Table/SubCategoriesTable.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
+use App\Model\Behavior\SwitchBehavior;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
@@ -29,6 +29,7 @@ use Cake\Validation\Validator;
  * @method \App\Model\Entity\SubCategory[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
  *
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ * @mixin SwitchBehavior
  */
 class SubCategoriesTable extends Table
 {
@@ -47,6 +48,7 @@ class SubCategoriesTable extends Table
         $this->setPrimaryKey('id');
 
         $this->addBehavior('Timestamp');
+        $this->addBehavior('Switch');
 
         $this->belongsTo('Categories', [
             'foreignKey' => 'category_id',
