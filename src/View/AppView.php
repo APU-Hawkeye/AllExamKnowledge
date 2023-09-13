c5172ee0e2396f1682c3a81347c69c50213c246c
Hawkeye <swarup@agpaytech.co.uk>
2023-09-13 14:39:07 +0530

    pagination helper loaded

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -41,15 +41,16 @@ class AppView extends View
      */
     public function initialize(): void
     {
-        $this->Paginator->setTemplates([
-            'nextActive' => '<li class="page-item"><a class="page-link" href="{{url}}"><i class="mdi mdi-chevron-right"></i></a></li>',
-            'nextDisabled' => '<li class="page-item disabled"><a class="page-link" disabled href="javascript:;"><i class="mdi mdi-chevron-right"></i></a></li>',
-            'prevActive' => '<li class="page-item"><a class="page-link" href="{{url}}"><i class="mdi mdi-chevron-left"></i></a></li>',
-            'prevDisabled' => '<li class="page-item disabled"><a class="page-link" disabled href="javascript:;"><i class="mdi mdi-chevron-left"></i></a></li>',
-            'number' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
-            'current' => '<li class="page-item disabled"><a class="page-link" disabled href="javascript:;">{{text}}</a></li>',
-        ]);
-        $this->loadHelper('PaginationControl');
+//        $this->Paginator->setTemplates([
+//            'nextActive' => '<li class="page-item"><a class="page-link" href="{{url}}"><i class="mdi mdi-chevron-right"></i></a></li>',
+//            'nextDisabled' => '<li class="page-item disabled"><a class="page-link" disabled href="javascript:;"><i class="mdi mdi-chevron-right"></i></a></li>',
+//            'prevActive' => '<li class="page-item"><a class="page-link" href="{{url}}"><i class="mdi mdi-chevron-left"></i></a></li>',
+//            'prevDisabled' => '<li class="page-item disabled"><a class="page-link" disabled href="javascript:;"><i class="mdi mdi-chevron-left"></i></a></li>',
+//            'number' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
+//            'current' => '<li class="page-item disabled"><a class="page-link" disabled href="javascript:;">{{text}}</a></li>',
+//        ]);
+//        $this->loadHelper('PaginationControl');
         $this->loadHelper("BackButton");
+        $this->loadHelper('Paginator', ['templates' => 'paginator-templates']);
     }
 }
