65c33f67b3819a88146ce7af859131168ea57f01
Hawkeye <swarup@agpaytech.co.uk>
2023-09-13 14:27:18 +0530

    paginator template added

---
+++ config/paginator-templates.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'nextActive' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}"><i data-feather="chevron-right"></i></a></li>',
+    'nextDisabled' => '<li class="page-item disabled"><a class="page-link text-primary" href="javascript:"><i data-feather="chevron-right"></i></a></li>',
+    'prevActive' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}"><i data-feather="chevron-left"></i></a></li>',
+    'prevDisabled' => '<li class="page-item disabled"><a class="page-link text-primary" href="javascript:"><i data-feather="chevron-left"></i></a></li>',
+    'first' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}">'.__("First").'</a></li>',
+    'last' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}">'.__("Last").'</a></li>',
+    'number' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}">{{text}}</a></li>',
+    'current' => '<li class="page-item active"><a class="page-link text-primary" href="javascript:">{{text}}</a></li>',
+    'ellipsis' => '',
+];
+?>
+
