2b04c454a85ed1c747cae69193e23a39a1f30d41
Hawkeye <swarup@agpaytech.co.uk>
2023-09-18 19:17:48 +0530

    switch behavior attached

--- src/Model/Table/JobNotificationsTable.php
+++ src/Model/Table/JobNotificationsTable.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
+use App\Model\Behavior\SwitchBehavior;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
@@ -24,6 +25,7 @@ use Cake\Validation\Validator;
  * @method \App\Model\Entity\JobNotification[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
  *
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ * @mixin SwitchBehavior
  */
 class JobNotificationsTable extends Table
 {
@@ -42,6 +44,7 @@ class JobNotificationsTable extends Table
         $this->setPrimaryKey('id');
 
         $this->addBehavior('Timestamp');
+        $this->addBehavior('Switch');
         $this->addBehavior('AttachmentFiles', [
             'fields' => ['file']
         ]);
