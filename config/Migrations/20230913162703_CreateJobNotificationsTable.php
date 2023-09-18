5a0b9eae4efa3c3a8557553909bc53903179ac05
Hawkeye <swarup@agpaytech.co.uk>
2023-09-18 13:18:55 +0530

    Job notification migration added

---
+++ config/Migrations/20230913162703_CreateJobNotificationsTable.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateJobNotificationsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('job_notifications', [
+            'id' => false,
+            'primary_key' => 'id'
+        ]);
+        $table
+            ->addColumn('id', 'uuid')
+            ->addColumn('title', 'text', [
+                'null' => false,
+            ])
+            ->addColumn('description', 'text', [
+                'null' => true,
+                'default' => null
+            ])
+            ->addColumn('tag', 'string', [
+                'null' => true,
+                'default' => null
+            ])
+            ->addColumn('link', 'text', [
+                'null' => true,
+                'default' => null
+            ])
+            ->addColumn('file', 'text')
+            ->addColumn('disabled', 'datetime', [
+                'null' => true,
+                'default' => null
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
+    }
+}
