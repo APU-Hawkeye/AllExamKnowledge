0b5d527fc68f81509b88e3c249ef7a4526fe81de
Hawkeye <swarup@agpaytech.co.uk>
2023-10-10 21:12:41 +0530

    paginator template modified

--- config/paginator-templates.php
+++ config/paginator-templates.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'nextActive' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}"><i data-feather="chevron-right"></i></a></li>',
-    'nextDisabled' => '<li class="page-item disabled"><a class="page-link text-primary" href="javascript:"><i data-feather="chevron-right"></i></a></li>',
-    'prevActive' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}"><i data-feather="chevron-left"></i></a></li>',
-    'prevDisabled' => '<li class="page-item disabled"><a class="page-link text-primary" href="javascript:"><i data-feather="chevron-left"></i></a></li>',
+    'nextActive' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}"><i class="bi bi-chevron-right"></i></a></li>',
+    'nextDisabled' => '<li class="page-item disabled"><a class="page-link text-primary" href="javascript:"><i class="bi bi-chevron-right"></i></a></li>',
+    'prevActive' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}"><i class="bi bi-chevron-left"></i></a></li>',
+    'prevDisabled' => '<li class="page-item disabled"><a class="page-link text-primary" href="javascript:"><i class="bi bi-chevron-left"></i></a></li>',
     'first' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}">'.__("First").'</a></li>',
     'last' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}">'.__("Last").'</a></li>',
     'number' => '<li class="page-item"><a class="page-link text-primary" href="{{url}}">{{text}}</a></li>',
